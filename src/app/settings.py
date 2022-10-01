654b4c1e8c61a71685a186f01ca0b00415d83986
iwason <j.iwamura1986@gmail.com>
2022-10-01 22:45:07 +0900

    CI 基本追加

--- src/app/settings.py
+++ src/app/settings.py
@@ -13,7 +13,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_local',
+        'USER': 'django_user',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'POST': '5432'
     }
 }
 
